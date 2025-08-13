20e3a91e73a88a8958ba03e6d7818fdb4720f7e8
Reindolf590 <150713702+Reindolf590@users.noreply.github.com>
2025-08-13 03:01:45 +0000

    Update index.tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,4 +1,4 @@
-mport { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -6,12 +6,8 @@ export default function Index() {
       <Text>Entry Screen - Awesome</Text>
 
       <View>
-        <Text style={styles.largeText}>
-          Typescript is great if you practice more
-        </Text>
-        <Text style={styles.mediumText}>
-          React Native provides you a single codebase for cross platforms
-        </Text>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
         <Text style={styles.smallText}>ALX is awesome</Text>
       </View>
     </View>
